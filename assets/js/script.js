91d3e324113efbb7dbd678f42267b5f2fe0fb7ff
MarieSophieGB <mariesophie.gb@gmail.com>
2022-12-07 16:13:24 +0100

    mise a jour branche

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Choix du Tab
+// Choix et affichage des Tabs
 
 const tabs = document.querySelectorAll('.nav-link')
 
@@ -33,9 +33,15 @@ window.addEventListener('click', event => {
     }
 });
 
+// Fonctions pour afficher les elements du container
 
 function pokeballs () {
-    document.querySelector('.row').innerHTML = "";
+    document.querySelector('.container').innerHTML = "";
+
+    let itemContainer = document.createElement('div');
+    itemContainer.className = "thomas";
+    itemContainer = document.querySelector('.container').appendChild(itemContainer);
+
     fetch('assets/json/store.json')
         .then(response => response.json())
         .then((data) => {
@@ -51,51 +57,63 @@ function pokeballs () {
                 let price = element.price;
 
                 let anchor = document.createElement('div');
-                anchor.innerHTML = '<div class="card text-center"><div class="card-body text-center" id='+id+'></div></div>';
-                anchor.className = 'col-sm-3';
-                
+                anchor.innerHTML = '<div class="card-body" id='+id+'>';
                 
                 if (category == 'Pokeballs') {
-
-                    anchor = document.querySelector('.row').appendChild(anchor);
-
-                    document.querySelector('.banner').innerHTML = ``
                     
+                    anchor.className = 'card card'+id+' cardthomas text-center';
+                    anchor = document.querySelector('.thomas').appendChild(anchor);
+
                     let cardId = document.getElementById(id);
                     
                     let cardImg = document.createElement('img');
-                    cardImg.className = 'cardImg '
                     cardImg.src = 'assets/img/'+image+'';
                     cardImg.alt = name;
+                    cardImg.className = "cardImg"
                     cardImg = cardId.appendChild(cardImg);
 
                     let cardName = document.createElement('p');
                     cardName.textContent = name;
-                    cardName.className = 'card-title';
+                    cardName.className = "card-title";
                     cardName = cardId.appendChild(cardName);
 
                     let cardOverview = document.createElement('p');
                     cardOverview.textContent = overview;
-                    cardOverview.className = 'card-text';
+                    cardOverview.className = "card-text"
                     cardOverview = cardId.appendChild(cardOverview);
 
                     let cardInput = document.createElement('input');
-                    cardInput.placeholder = 'Quantité';
+                    cardInput.placeholder = 'Quantity';
                     cardInput.minLength = 1;
                     cardInput.maxLength = 2;
                     cardInput = cardId.appendChild(cardInput);
 
+                    let cardAdd = document.createElement('button');
+                    cardAdd.dataset.name = name;
+                    cardAdd.dataset.image = image;
+                    cardAdd.dataset.price = price;
+                    cardAdd.className = 'btn btn-primary add';
+                    cardAdd.type = 'button';
+                    cardAdd.textContent = "Add";
+                    cardAdd = cardId.appendChild(cardAdd);
+
                     let cardPrice = document.createElement('div');
-                    cardPrice.className = 'card-footer'
                     cardPrice.textContent = price+'¥';
-                    cardPrice = cardId.appendChild(cardPrice);
+                    cardPrice.className = "card-footer"
+                    cardPrice = document.querySelector('.card'+id+'').appendChild(cardPrice);
                 }
             });
         });
 };
 
+
 function battleItems () {
-    document.querySelector('.row').innerHTML = "";
+    document.querySelector('.container').innerHTML = "";
+
+    let itemContainer = document.createElement('div');
+    itemContainer.className = "thomas";
+    itemContainer = document.querySelector('.container').appendChild(itemContainer);
+
     fetch('assets/json/store.json')
         .then(response => response.json())
         .then((data) => {
@@ -110,51 +128,65 @@ function battleItems () {
                 let overview = element.overview;
                 let price = element.price;
 
+                
+
                 let anchor = document.createElement('div');
-                anchor.innerHTML = '<div class="card text-center"><div class="card-body text-center" id='+id+'></div></div>';
-                anchor.className = 'col-sm-3';
+                anchor.innerHTML = '<div class="card-body" id='+id+'>';
                 
                     if (category == 'Battle items') {
 
-                        document.querySelector('.banner').innerHTML = ``
+                    anchor.className = 'card card'+id+' cardthomas text-center';
+                    anchor = document.querySelector('.thomas').appendChild(anchor);
 
-                        anchor = document.querySelector('.row').appendChild(anchor);
-    
-                        let cardId = document.getElementById(id);
-
-                        let cardImg = document.createElement('img');
-                    cardImg.className = 'cardImg '
+                    let cardId = document.getElementById(id);
+                    
+                    let cardImg = document.createElement('img');
                     cardImg.src = 'assets/img/'+image+'';
                     cardImg.alt = name;
+                    cardImg.className = "cardImg"
                     cardImg = cardId.appendChild(cardImg);
 
                     let cardName = document.createElement('p');
                     cardName.textContent = name;
-                    cardName.className = 'card-title';
+                    cardName.className = "card-title";
                     cardName = cardId.appendChild(cardName);
 
                     let cardOverview = document.createElement('p');
                     cardOverview.textContent = overview;
-                    cardOverview.className = 'card-text';
+                    cardOverview.className = "card-text"
                     cardOverview = cardId.appendChild(cardOverview);
 
                     let cardInput = document.createElement('input');
-                    cardInput.placeholder = 'Quantité';
+                    cardInput.placeholder = 'Quantity';
                     cardInput.minLength = 1;
                     cardInput.maxLength = 2;
                     cardInput = cardId.appendChild(cardInput);
 
+                    let cardAdd = document.createElement('button');
+                    cardAdd.dataset.name = name;
+                    cardAdd.dataset.image = image;
+                    cardAdd.dataset.price = price;
+                    cardAdd.className = 'btn btn-primary add';
+                    cardAdd.type = 'button';
+                    cardAdd.textContent = "Add";
+                    cardAdd = cardId.appendChild(cardAdd);
+
                     let cardPrice = document.createElement('div');
-                    cardPrice.className = 'card-footer'
                     cardPrice.textContent = price+'¥';
-                    cardPrice = cardId.appendChild(cardPrice);
+                    cardPrice.className = "card-footer"
+                    cardPrice = document.querySelector('.card'+id+'').appendChild(cardPrice);
                     }
                 });
             });
     };
 
 function medicine () {
-    document.querySelector('.row').innerHTML = "";
+    document.querySelector('.container').innerHTML = "";
+
+    let itemContainer = document.createElement('div');
+    itemContainer.className = "thomas";
+    itemContainer = document.querySelector('.container').appendChild(itemContainer);
+
     fetch('assets/json/store.json')
         .then(response => response.json())
         .then((data) => {
@@ -170,49 +202,62 @@ function medicine () {
                 let price = element.price;
 
                 let anchor = document.createElement('div');
-                anchor.innerHTML = '<div class="card text-center"><div class="card-body text-center" id='+id+'></div></div>';
-                anchor.className = 'col-sm-3';
+                anchor.innerHTML = '<div class="card-body" id='+id+'>';
 
                     if (category == 'Medicine') {
-                        document.querySelector('.banner').innerHTML = ``
 
-                        anchor = document.querySelector('.row').appendChild(anchor);
+                        anchor.className = 'card card'+id+' cardthomas text-center';
+                        anchor = document.querySelector('.thomas').appendChild(anchor);
     
                         let cardId = document.getElementById(id);
-
+                        
                         let cardImg = document.createElement('img');
-                        cardImg.className = 'cardImg '
                         cardImg.src = 'assets/img/'+image+'';
                         cardImg.alt = name;
+                        cardImg.className = "cardImg"
                         cardImg = cardId.appendChild(cardImg);
-
+    
                         let cardName = document.createElement('p');
                         cardName.textContent = name;
-                        cardName.className = 'card-title';
+                        cardName.className = "card-title";
                         cardName = cardId.appendChild(cardName);
-
+    
                         let cardOverview = document.createElement('p');
                         cardOverview.textContent = overview;
-                        cardOverview.className = 'card-text';
+                        cardOverview.className = "card-text"
                         cardOverview = cardId.appendChild(cardOverview);
-
+    
                         let cardInput = document.createElement('input');
-                        cardInput.placeholder = 'Quantité';
+                        cardInput.placeholder = 'Quantity';
                         cardInput.minLength = 1;
                         cardInput.maxLength = 2;
                         cardInput = cardId.appendChild(cardInput);
-
+    
+                        let cardAdd = document.createElement('button');
+                        cardAdd.dataset.name = name;
+                        cardAdd.dataset.image = image;
+                        cardAdd.dataset.price = price;
+                        cardAdd.className = 'btn btn-primary add';
+                        cardAdd.type = 'button';
+                        cardAdd.textContent = "Add";
+                        cardAdd = cardId.appendChild(cardAdd);
+    
                         let cardPrice = document.createElement('div');
-                        cardPrice.className = 'card-footer'
                         cardPrice.textContent = price+'¥';
-                        cardPrice = cardId.appendChild(cardPrice);
+                        cardPrice.className = "card-footer"
+                        cardPrice = document.querySelector('.card'+id+'').appendChild(cardPrice);
                     }
                 });
             });
     };
 
 function TMs () {
-    document.querySelector('.row').innerHTML = "";
+    document.querySelector('.container').innerHTML = "";
+
+    let itemContainer = document.createElement('div');
+    itemContainer.className = "thomas";
+    itemContainer = document.querySelector('.container').appendChild(itemContainer);
+
     fetch('assets/json/store.json')
         .then(response => response.json())
         .then((data) => {
@@ -228,94 +273,107 @@ function TMs () {
                 let price = element.price;
 
                 let anchor = document.createElement('div');
-                anchor.innerHTML = '<div class="card text-center"><div class="card-body text-center" id='+id+'></div></div>';
-                anchor.className = 'col-sm-3';
+                anchor.innerHTML = '<div class="card-body" id='+id+'>';
 
                 if (category == 'TMs') {
-                    document.querySelector('.banner').innerHTML = ``
 
-                    anchor = document.querySelector('.row').appendChild(anchor);
+                    anchor.className = 'card card'+id+' cardthomas text-center';
+                    anchor = document.querySelector('.thomas').appendChild(anchor);
 
                     let cardId = document.getElementById(id);
-
+                    
                     let cardImg = document.createElement('img');
-                    cardImg.className = 'cardImg '
                     cardImg.src = 'assets/img/'+image+'';
                     cardImg.alt = name;
+                    cardImg.className = "cardImg1"
                     cardImg = cardId.appendChild(cardImg);
 
                     let cardName = document.createElement('p');
                     cardName.textContent = name;
-                    cardName.className = 'card-title';
+                    cardName.className = "card-title";
                     cardName = cardId.appendChild(cardName);
 
                     let cardOverview = document.createElement('p');
                     cardOverview.textContent = overview;
-                    cardOverview.className = 'card-text';
+                    cardOverview.className = "card-text"
                     cardOverview = cardId.appendChild(cardOverview);
 
                     let cardInput = document.createElement('input');
-                    cardInput.placeholder = 'Quantité';
+                    cardInput.placeholder = 'Quantity';
                     cardInput.minLength = 1;
                     cardInput.maxLength = 2;
                     cardInput = cardId.appendChild(cardInput);
 
+                    let cardAdd = document.createElement('button');
+                    cardAdd.dataset.name = name;
+                    cardAdd.dataset.image = image;
+                    cardAdd.dataset.price = price;
+                    cardAdd.className = 'btn btn-primary add';
+                    cardAdd.type = 'button';
+                    cardAdd.textContent = "Add";
+                    cardAdd = cardId.appendChild(cardAdd);
+
                     let cardPrice = document.createElement('div');
-                    cardPrice.className = 'card-footer'
                     cardPrice.textContent = price+'¥';
-                    cardPrice = cardId.appendChild(cardPrice);
+                    cardPrice.className = "card-footer"
+                    cardPrice = document.querySelector('.card'+id+'').appendChild(cardPrice);
                 }
                 });
             });
     };
 
-
-
-
-//     console.log(document.querySelector(element).classList != 'active');
-//     if (document.querySelector(element).classList != 'active') {
-//         document.querySelector('.tab').id = 'landing';
-//     }
-
-
-
-//     // document.querySelector(element).addEventListener('click', () => {
-//     //     if (element.classList != 'active') {
-//     //         element. = 'active';
-
-//     //     }
-//     // });
-// });
-
-
-
+function News () {
+    document.querySelector('.container').innerHTML = "";
+}
 
 // UX
 
-// let regex = /^[1-9]{1}[0-9]{0,1}$/;
+let regex = /^[1-9]{1}[0-9]{0,1}$/;
 
 // window.addEventListener('keydown', element => {
 //     if ((element.key === 'Enter')) {
 //         if (regex.test(element.target.value)) {
-//             console.log('hello')
-//             quantity = element.target.value;
-//             console.log(quantity);
-            
-
-
-
-
-
-
-
-
-
-
+//             let quantity = element.target.value;
+//             let id = element.target.parentNode.id;
+//             element.target.value = "";
 //         } 
 //         else {
-//             message.textContent = "Veuillez saisir un élément valide";
+//             console.log("Veuillez saisir un élément valide");
+//             // message.textContent = "Veuillez saisir un élément valide";
 //         };
 //     };
 // });
 
-// Modal
+
+window.addEventListener('click', element => {
+    if ((element.target.classList.contains('add'))) {
+        if (regex.test(element.target.previousSibling.value)) {
+
+            let id = element.target.parentNode.id;
+            let quantity = element.target.previousSibling.value;
+
+            console.log(id);
+            console.log(element.target.dataset.name);
+            console.log(element.target.dataset.image);
+            console.log(element.target.dataset.price);
+            console.log(quantity)
+
+            let modal = document.querySelector('.modal-body');
+            let modalLine = document.createElement('li');
+            modalLine.innerHTML = `
+            <div><img class="cartItem" src="assets/img/${element.target.dataset.image}"/></div><div class="h5 text">${element.target.dataset.name}</div>${element.target.dataset.price}¥<div>x${quantity}</div><button type="button" class="btn"><img class="Editer" src="https://img.icons8.com/color-glass/38/null/edit--v1.png"/></button><button type="button" class="btn"><img class="Supprimer" src="https://img.icons8.com/fluency/38/null/delete-forever.png"/></button></li>`;
+            modalLine = modal.appendChild(modalLine);
+
+            
+        } 
+        else {
+            console.log("Veuillez saisir un élément valide");
+            // message.textContent = "Veuillez saisir un élément valide";
+        };
+    };
+});
+
+
+
+
+
